4b0d7283870fa07451e3df9e6bd1132ad009a445
devkush0999 <devesh2cse@gmail.com>
2024-11-23 22:32:19 +0530

    setup

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { Text, ScrollView, View, StyleSheet } from 'react-native';
+import React from 'react';
+import Categories from '../Components/Categories';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Restaurants from '../Components/Restaurants';
+import Colors from '@/constants/Colors';
+
+const Page = () => { 
+  return (
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={{ paddingBottom: 40 }}>
+        <Categories />
+        <Text style={styles.header}>Top picks in your neighbourhood.</Text>
+        <Restaurants />
+        <Text style={styles.header}>Offers near you</Text>
+        <Restaurants />
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 16,
+    top: 100,
+    backgroundColor: Colors.lightGrey,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 16,
+    marginBottom: 8,
+    paddingHorizontal: 16,
+  },
+});
+
+export default Page;
